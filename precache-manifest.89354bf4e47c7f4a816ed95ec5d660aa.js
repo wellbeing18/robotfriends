37a6833e7d6e4a06c54ec4ab6f690cc0a0789766
wellbeing18 <wang.618.bin@gmail.com>
2020-05-01 20:39:15 -0400

    Updates

---
+++ precache-manifest.89354bf4e47c7f4a816ed95ec5d660aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8c7acde579d5373f52d4633a830569",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "537e8a2237e1150a84cd",
+    "url": "/robotfriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "14c199e47e4e8bbb5d8b",
+    "url": "/robotfriends/static/css/main.840ee446.chunk.css"
+  },
+  {
+    "revision": "537e8a2237e1150a84cd",
+    "url": "/robotfriends/static/js/2.723846ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotfriends/static/js/2.723846ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c199e47e4e8bbb5d8b",
+    "url": "/robotfriends/static/js/main.1389edec.chunk.js"
+  },
+  {
+    "revision": "b5dcd7916ed0fc145a89",
+    "url": "/robotfriends/static/js/runtime-main.a23e58ab.js"
+  }
+]);
